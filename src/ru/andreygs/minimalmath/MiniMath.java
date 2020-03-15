faee64c28eb883e009a2560bdbd93f1c0c0d6743
AndreyGS <ukbpyh@gmail.com>
2020-03-15 23:05:10 +0300

    All functionality of working with double and surrogates is moved to new class SlashedDouble. Added rounding, and now final number is exactly as it is in a native Math.pow() function. Also bug in innerMult with shifting by negative value was fixed, and it and inner pow() method are working as they should, allmost... After making some test, a new bug was introduced, in rare cases there is a rounding problem (with big degrees there is a divergence in somewhere about 1.5 times). It's subject to nearest work with.

--- src/ru/andreygs/minimalmath/MiniMath.java
+++ src/ru/andreygs/minimalmath/MiniMath.java
@@ -1,5 +1,7 @@
 package ru.andreygs.minimalmath;
 
+import static java.lang.System.*;
+
 public class MiniMath {
 	private static int[] intPwr = 
 	{
@@ -23,72 +25,94 @@ public class MiniMath {
 	}
 	
 	public static Double pow(double number, double power) {
-		number = intPower(number, power);
-		return 0.0;
+		SlashedDouble sdnum = new SlashedDouble(number);
+		SlashedDouble sdpow = new SlashedDouble(power, true);
+		
+		return intPower(sdnum, sdpow).getIEEE754();
 	}
 	
-	private static double intPower(double number, double power) {
-		int ipwr = (int) power;
-		double result = 1.0;
+	private static SlashedDouble intPower(SlashedDouble number, SlashedDouble power) {
+		int ipwr = (int) power.getIntMantissaInteger();
+		SlashedDouble result = new SlashedDouble(1.0);
 		for (int i = 0; i < intPwr.length; i++) {
 			if ((ipwr & intPwr[i]) == intPwr[i]) {
-				System.out.println(result);
-				result = MiniMath.innerMult(number, result);
+				result = innerMult(number, result, "");
 			}
-			result = MiniMath.innerMult(result, result);
+			result = innerMult(result, result, "");
 		}
 		if ((ipwr & 1) == 1) {
-			result = MiniMath.innerMult(number, result);
+			result = innerMult(number, result, "");
 		}
-		
+		if (number.isNegative() && ipwr % 2 == 1) result.setSign("-");
 		return result;
 	}
 	
-	public static double innerMult(double number1, double number2) {
+	public static Double mult(double number1, double number2) {
 		if (number1 == 1) return number2;
 		if (number2 == 1) return number1;
-		if (number1 == 0 || number2 == 0) return 0;
+		if (number1 == 0 || number2 == 0) return 0.0;
 		if (number1 == 0xffffffff) return (double)~((long)number2) + 1;
 		if (number2 == 0xffffffff) return (double)~((long)number2) + 1;
 		
-		boolean
-			isnegative;
+		SlashedDouble sd1 = new SlashedDouble(number1);
+		SlashedDouble sd2 = new SlashedDouble(number2);
+		String
+			negativesign;
 		if ((number1 >= 0 && number2 >= 0) || (number1 < 0 && number2 < 0)) {
-			isnegative = false;
+			negativesign = "";
 		} else {
-			isnegative = true;
+			negativesign = "-";
 		}
-		
-		String[] 
-			num1array = Double.toHexString(number1).split("[.p]"), 
-			num2array = Double.toHexString(number2).split("[.p]");
-		String 
-			num1raw = cutFractTail('1' + fromHexToBinary("0x" + num1array[1])),
-			num2raw = cutFractTail('1' + fromHexToBinary("0x" + num2array[1]));
-			System.out.println(num1raw);
-			System.out.println(num2raw);
-		if (onesEnum(num1raw) < onesEnum(num2raw)) {
-			String[] temparray = num1array; num1array = num2array; num2array = temparray;
-			String tempraw = num1raw; num1raw = num2raw; num2raw = tempraw;
-		}
+		return innerMult(sd1, sd2, negativesign).getIEEE754();
+	}
 	
-		int
-			biasresult = 0;
-		long
-			unit = Long.valueOf(num1raw, 2),
-			tempresult = 0L;
-		
-		for (int i = num2raw.length() + 0xffffffff, shift = 0, check, spaceneed, leadzeroes; i > 0xffffffff; i += 0xffffffff, shift++) {
+	private static SlashedDouble innerMult(SlashedDouble number1, SlashedDouble number2, String negativesign) {
+		long result = 0l, unit;
+		String num2raw;
+		if (number1.onesEnum() < number2.onesEnum()) {
+			unit = number2.getLongMantissa();
+			num2raw = number1.getBinaryRaw();
+		} else {
+			unit = number1.getLongMantissa();
+			num2raw = number2.getBinaryRaw();
+		}
+
+		int biasresult = 0; 
+
+		for (int i = num2raw.length() + 0xffffffff, shift = 0, check, spaceneed, leadzeroes; i > 0xffffffff; i+= 0xffffffff, shift++) {
 			if (num2raw.charAt(i) == '1') {
 				leadzeroes = Long.numberOfLeadingZeros(unit);
 				check = leadzeroes + ~shift;
-				if (check > 0xffffffff) {
+				if (check > 0) {
 					unit <<= shift;
 				} else {
-					unit <<= leadzeroes + 0xffffffff;
+					if (leadzeroes == 0) {
+						unit >>>= 1;
+						spaceneed = ~check + 1;
+						if (result > 0) result >>>= 1;
+						biasresult += shift + 1;
+					} else {
+						unit <<= leadzeroes + 0xffffffff;
+						spaceneed = ~check + 1;
+						result >>>= spaceneed;
+						biasresult += spaceneed;
+					}
+					
+					/*
+					// this is a unnecessary rounding codition, but in some cases it can bring
+					// some additionally accuracy
 					spaceneed = ~check + 1;
-					tempresult >>>= spaceneed;
+					result >>>= spaceneed;
 					biasresult += spaceneed;
+					if (Long.numberOfTrailingZeros(result) == 0) {
+						result >>>= 1;
+						result++;
+					} else {
+						result >>>= 1;
+					}
+					biasresult += spaceneed;
+					*/
+					
 					/*
 					// this is another version of shifting statements
 					// it was made for propose of theroreticaly accuracy increase
@@ -106,167 +130,31 @@ public class MiniMath {
 					}
 					*/
 				}
-				tempresult += unit;
+				result += unit;
 				shift = 0;
 			}
 		}
-		String
-			stempresult = Long.toBinaryString(tempresult);
-		int
-			resultexp = getExponent(stempresult, num1array[2], num2array[2], num1raw, num2raw, biasresult);
-		return getIEEE754(stempresult, resultexp, isnegative);
+		String mantissa = Long.toBinaryString(result);
+		int resultexp = getExponent(mantissa, number1.getExp(), number2.getExp(), number1.getBinaryRaw(), number2.getBinaryRaw(), biasresult);
+		return new SlashedDouble(mantissa, resultexp, negativesign, result);
 	}
 	
-	private static String fromHexToBinary(String number) {
-		String raw = "";
-		for (int i = 2; i < number.length(); i++) {
-			char digit = number.charAt(i);
-			switch(digit) {
-				case '0': raw += "0000"; break;
-				case '1': raw += "0001"; break;
-				case '2': raw += "0010"; break;
-				case '3': raw += "0011"; break;
-				case '4': raw += "0100"; break;
-				case '5': raw += "0101"; break;
-				case '6': raw += "0110"; break;
-				case '7': raw += "0111"; break;
-				case '8': raw += "1000"; break;
-				case '9': raw += "1001"; break;
-				case 'a': raw += "1010"; break;
-				case 'b': raw += "1011"; break;
-				case 'c': raw += "1100"; break;
-				case 'd': raw += "1101"; break;
-				case 'e': raw += "1110"; break;
-				default: raw += "1111"; break;
-			}
-		}
-		return raw;
-	}
-	
-	private static String cutFractTail(String number) {
-		for (int i = number.length() + 0xffffffff; i > 0xffffffff; i += 0xffffffff) {
-			if (number.charAt(i) == '1') {
-				return number.substring(0, i+1);
-			}
-		}
-		return number;
-	}
-	
-	private static int onesEnum(String number) {
-		int counter = 0;
-		for (int i = 0; i < number.length(); i++) {
-			if (number.charAt(i) == '1') counter++;
-		}
-		return counter;
-	}
-	
-	private static int getExponent(String number, String startexp1, String startexp2, String num1raw, String num2raw, int biasresult) {
-		int 
-			istartexp1 = Integer.parseInt(startexp1), 
-			istartexp2 = Integer.parseInt(startexp2),
-			fractdigitsnum = num1raw.length() + num2raw.length() + 0xfffffffe;
-		return number.length() + ~fractdigitsnum + istartexp1 + istartexp2 + biasresult;
-	}
-
-	public static String getStringIEEE754(String number, int exp, boolean isnegative) {
-		String ieee754;
-		if (isnegative) ieee754 = "1";
-		else ieee754 = "0";
-		exp += 1023;
-		ieee754 += Integer.toBinaryString(exp) + number.substring(1);
-		if (ieee754.length() < 64) {
-			for (int i = ieee754.length(); i < 64; i++) {
-				ieee754 += "0";
-			}
-		} else {
-			ieee754 = ieee754.substring(0, 64);
-		}
-		return ieee754;	
-	}
-	
-	public static double getIEEE754(String number, int exp) {
-		return getIEEE754(number, exp, false);	
-	}
-	
-	public static double getIEEE754(String number, int exp, boolean isnegative) {
-		if (number.indexOf('1') == 0xffffffff && exp == 0) return 0.0;
-		String ieee754 = "";
-		if (isnegative) ieee754 = "-";
-		ieee754 += "0x1." + fromBinaryToHex(number.substring(1)) + "p" + exp;
-		return Double.valueOf(ieee754);	
-	}
-	
-	public static String fromBinaryToHex(String number) {
-		if (number.equals("")) return "0";
-		String raw = "", digit;
-		if (number.length() < 52) {
-			for (int i = number.length(); i % 4 != 0; i++) {
-				number += "0";
-			}
-		}
-		for (int i = 0; i < 52; i += 4) {
-			digit = number.substring(i, i+4);
-			switch(digit) {
-				case "0000": raw += "0"; break;
-				case "0001": raw += "1"; break;
-				case "0010": raw += "2"; break;
-				case "0011": raw += "3"; break;
-				case "0100": raw += "4"; break;
-				case "0101": raw += "5"; break;
-				case "0110": raw += "6"; break;
-				case "0111": raw += "7"; break;
-				case "1000": raw += "8"; break;
-				case "1001": raw += "9"; break;
-				case "1010": raw += "a"; break;
-				case "1011": raw += "b"; break;
-				case "1100": raw += "c"; break;
-				case "1101": raw += "d"; break;
-				case "1110": raw += "e"; break;
-				default: raw += "f"; break;
-			}
-		}
-		return raw;
+	private static int getExponent(String fraction, int startexp1, int startexp2, String num1raw, String num2raw, int biasresult) {
+		int fractdigitsnum = num1raw.length() + num2raw.length() + 0xfffffffe;
+		return fraction.length() + ~fractdigitsnum + startexp1 + startexp2 + biasresult;
 	}
 	
 	public static void main(String[] args) {
-		//innerMult(-1123423424.523, 2.4345345422554);
-		System.out.println(-1123423424.523 * 2.4345345422554);
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(-2.735013132580096E9)));
 		System.out.println(Double.toHexString(1));
 		System.out.println(Double.toHexString(0));
-		System.out.println(innerMult(-11.123225, 2.4345345422554));
-		System.out.println(-11.123225 * 2.4345345422554);
-		//System.out.println(intPower(234.532, 40));
-		//System.out.println(Math.pow(234.532, 40));
-		//System.out.println(innerMult(-1, 15));
-		/*
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(1)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(2)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(3)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(4)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(5)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(6)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(7)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(8)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(9)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(10)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(11)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(12)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(13)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(14)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(15)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(16.0)));
-		System.out.println();
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.5)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.25)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.75)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.125)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.625)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.375)));
-		System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.875)));
-		//System.out.println(Long.toBinaryString(Double.doubleToLongBits(0.0625)));
-		*/
-		//System.out.println(Double.toHexString(1.25));
-		//MiniMath.pow(5, 4.9);
+		System.out.println(pow(234.532, 40));
+		System.out.println(Math.pow(234.532, 40));
+		
+		for (int i = 0; i < 10; i++) {
+			double d1 = Math.random()*100, d2 = Math.floor(Math.random()*100);
+			
+			System.out.println(pow(d1, d2));
+			System.out.println(Math.pow(d1, d2));
+		}
 	}
 }
---
+++ src/ru/andreygs/minimalmath/SlashedDouble.java
@@ -0,0 +1,279 @@
+package ru.andreygs.minimalmath;
+
+public class SlashedDouble {
+	
+	private String negativesign;
+	private String raw;
+	private Integer exp;
+	
+	private Integer onesnum;
+	
+	private Long mantissa;
+	
+	private String roundedbin;
+	private String roundedhex;
+	
+	private String ieee754bin;
+	private String ieee754hex;
+	
+	private Double number;
+	
+	private String intraw;
+	private String fractraw;
+	
+	private Long intmantissalong;
+	private Integer intmantissaint;
+	private Long fractmantissa;
+	
+	public SlashedDouble(double number) {
+		this.number = number;
+		slashIt();
+	}
+	
+	public SlashedDouble(double number, boolean additionalslicing) {
+		this(number);
+		getIntRaw();
+		getFractRaw();
+	}
+	
+	public SlashedDouble(Long mantissa, Integer exp, String negativesign) {
+		this.mantissa = mantissa;
+		this.exp = exp;
+		this.negativesign = negativesign;
+	}
+	
+	public SlashedDouble(String raw, Integer exp, String negativesign) {
+		this.raw = raw;
+		this.exp = exp;
+		this.negativesign = negativesign;
+	}
+	
+	public SlashedDouble(String raw, Integer exp, String negativesign, Long mantissa) {
+		this(raw, exp, negativesign);
+		this.mantissa = mantissa;
+	}
+	
+	private void slashIt() {
+		String[] stripes = Double.toHexString(number).split("[.p]");
+		if (stripes[0].charAt(0) == '-') negativesign = "";
+		else negativesign = "";
+		roundedhex = stripes[1];
+		raw = "1" + cutFractTail(fromHexToBinary(roundedhex));
+		exp = Integer.valueOf(stripes[2]);
+	}
+	
+	private static String fromHexToBinary(String hexraw) {
+		String raw = "";
+		for (int i = 0; i < hexraw.length(); i++) {
+			char digit = hexraw.charAt(i);
+			switch(digit) {
+				case '0': raw += "0000"; break;
+				case '1': raw += "0001"; break;
+				case '2': raw += "0010"; break;
+				case '3': raw += "0011"; break;
+				case '4': raw += "0100"; break;
+				case '5': raw += "0101"; break;
+				case '6': raw += "0110"; break;
+				case '7': raw += "0111"; break;
+				case '8': raw += "1000"; break;
+				case '9': raw += "1001"; break;
+				case 'a': raw += "1010"; break;
+				case 'b': raw += "1011"; break;
+				case 'c': raw += "1100"; break;
+				case 'd': raw += "1101"; break;
+				case 'e': raw += "1110"; break;
+				default: raw += "1111"; break;
+			}
+		}
+		return raw;
+	}
+	
+	public static String fromBinaryToHex(String raw) {
+		if (raw.equals("")) return "0";
+		String hexraw = "", digit;
+		if (raw.length() < 52) {
+			for (int i = raw.length(); i % 4 != 0; i++) {
+				raw += "0";
+			}
+		}
+		for (int i = 0; i < 52; i += 4) {
+			digit = raw.substring(i, i+4);
+			switch(digit) {
+				case "0000": hexraw += "0"; break;
+				case "0001": hexraw += "1"; break;
+				case "0010": hexraw += "2"; break;
+				case "0011": hexraw += "3"; break;
+				case "0100": hexraw += "4"; break;
+				case "0101": hexraw += "5"; break;
+				case "0110": hexraw += "6"; break;
+				case "0111": hexraw += "7"; break;
+				case "1000": hexraw += "8"; break;
+				case "1001": hexraw += "9"; break;
+				case "1010": hexraw += "a"; break;
+				case "1011": hexraw += "b"; break;
+				case "1100": hexraw += "c"; break;
+				case "1101": hexraw += "d"; break;
+				case "1110": hexraw += "e"; break;
+				default: hexraw += "f"; break;
+			}
+		}
+		return hexraw;
+	}
+	
+	public static String cutFractTail(String raw) {
+		for (int i = raw.length() + 0xffffffff; i > 0xffffffff; i += 0xffffffff) {
+			if (raw.charAt(i) == '1') {
+				return raw.substring(0, i+1);
+			}
+		}
+		return raw;
+	}
+	
+	public Long getLongMantissa() {
+		if (mantissa == null) {
+			mantissa = Long.valueOf(raw, 2);
+		}
+		return mantissa;
+	}
+	
+	public Integer getExp() {
+		return exp;
+	}
+	
+	public String getBinaryRaw() {
+		return raw;
+	}
+	
+	public String getRoundedHex() {
+		if (roundedhex == null) {
+			roundedhex = fromBinaryToHex(roundedbin);
+		}
+		return roundedhex;
+	}
+	
+	private String roundingToDoubleRaw() {
+		if (roundedbin == null) {
+			if (raw.length() > 53) {
+				if (raw.charAt(53) == '1') {
+					long chunk = Long.valueOf(raw.substring(1,53), 2);
+					chunk++;
+					roundedbin = Long.toBinaryString(chunk);
+				} else {
+					roundedbin = raw.substring(1,53);
+				}
+			} else {
+				roundedbin = raw.substring(1);
+				for (int i = roundedbin.length(); i < 52; i++) {
+					roundedbin += '0';
+				}
+			}
+		}
+		
+		return roundedbin;
+	} 
+	
+	public String getDoubleRaw() {
+		if (ieee754bin == null) {
+			if (negativesign == "-") ieee754bin = "1";
+			else ieee754bin = "0";
+			ieee754bin += Integer.toBinaryString(exp + 1023) + roundingToDoubleRaw();
+		}
+		return ieee754bin;	
+	}
+	
+	public String getDoubleHexRaw() {
+		if (ieee754hex == null) {
+			if (getDoubleRaw().indexOf('1') == 0xffffffff && exp == 0) {
+				ieee754hex = "0x0.0p0";
+			} else {
+				ieee754hex = negativesign + "0x1." + getRoundedHex() + "p" + exp;
+			}
+		}
+		return ieee754hex;
+	}
+	
+	public Double getIEEE754() {
+		if (number == null) {
+			number = Double.valueOf(getDoubleHexRaw());
+		}
+		return number;	
+	}
+	
+	public Integer onesEnum() {
+		if (onesnum == null) {
+			int counter = 0;
+			for (int i = 0; i < getBinaryRaw().length(); i++) {
+				if (raw.charAt(i) == '1') counter++;
+			}
+			onesnum = counter;
+		}
+		return onesnum;
+	}
+	
+	public String getIntRaw() {
+		if (intraw == null) {
+			if (exp < 0) {
+				intraw = "";
+			} else {
+				if (exp < raw.length()) {
+					intraw = raw.substring(0, exp+1);
+				} else {
+					intraw = raw;
+					for (int i = intraw.length(); i < 64 && i <= exp; i++) {
+						intraw += '0';
+					}
+				}
+			}
+		}
+		return intraw;
+	}
+	
+	public String getFractRaw() {
+		if (fractraw == null) {
+			if (getIntRaw().length() < raw.length()) {
+				fractraw = raw.substring(getIntRaw().length());
+				for (int i = fractraw.length(); i < 64; i++) {
+					fractraw += '0';
+				}
+			} else {
+				fractraw = "0000000000000000000000000000000000000000000000000000000000000000";
+			}
+			
+		}
+		return fractraw;
+	}
+	
+	public Long getIntMantissaLong() {
+		if (intmantissalong == null) {
+			intmantissalong = Long.valueOf(intraw, 2);
+		}
+		return intmantissalong;
+	}
+	
+	public Integer getIntMantissaInteger() {
+		if (intmantissaint == null) {
+			if (intraw.length() > 32) {
+				intmantissaint = Integer.valueOf(intraw.substring(0, 32), 2);
+			} else {
+				intmantissaint = Integer.valueOf(intraw, 2);
+			}
+		}
+		return intmantissaint;
+	}
+	
+	public Long getFractMantissa() {
+		if (fractmantissa == null) {
+			fractmantissa = Long.valueOf(fractraw, 2);
+		}
+		return fractmantissa;
+	}
+	
+	public boolean isNegative() {
+		if (negativesign == "-") return true;
+		else return false;
+	}
+	
+	public void setSign(String sign) {
+		if (sign == "" || sign == "-") negativesign = sign;
+	}
+}
